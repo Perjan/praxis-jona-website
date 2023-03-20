38e0c44eeebdae0d3adcc59ddb7478f1d82a81a1
Perjan Duro <perjan.duro@me.com>
2023-03-20 16:23:09 +0100

    Updates stuff

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -2,6 +2,6 @@ import MajorFeatureSection from "../MajorFeatureSection"
 
 export default function Features() {
     return (
-        <MajorFeatureSection />
+        <MajorFeatureSection imageUrl="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" />
     )
 }
