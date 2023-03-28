8cfcd30973afa3130d3450b52d699e744275759d
Krist Duro <krist.duro@icloud.com>
2023-03-28 11:26:01 +0200

    adds faqs

--- FAQs.jsx
+++ FAQs.jsx
@@ -1,16 +1,70 @@
 const faqs = [
     {
       id: 1,
-      question: "What's the best thing about Switzerland?",
+      question: "What is MoneyCoach?",
       answer:
-        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        "MoneyCoach is a modern cash and budget tracker. Designed to give you a grip on your personal finances. It helps you save more and spend smarter. And rewards you for reaching your goals.",
     },
     {
         id: 2,
-        question: "What's the best thing about Switzerland?",
+        question: "Is MoneyCoach Private",
         answer:
-          "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
-      },
+          "Absolutely. We have the utmost respect for your privacy. Your information is safe and secure with MoneyCoach. And it stays on your device.",
+    },
+    {
+      id: 3,
+      question: "What currencies are available?",
+      answer:
+        "All of them. From the American Dollar to the Albanian lek. (Premium Feature)",
+    },
+    {
+        id: 4,
+        question: "How secure is MoneyCoach?",
+        answer:
+          "Very secure. MoneyCoach is secured using Passcode, Touch ID or FaceID, so you can sleep safely… that is if Ethan Hunt is not around.",
+    },
+    {
+      id: 5,
+      question: "How much does it cost?",
+      answer:
+        "We offer MoneyCoach Premium for our Pro users. If you are a normal user, you can still get all the most used features for free. But if you want to support future development and the team, you can subscribe to MoneyCoach Premium and unlock extra powerful features.",
+    },
+    {
+      id: 6,
+      question: "Are there any ads?",
+      answer:
+        "Nope. We hate ads, just like you do.",
+    },
+    {
+      id: 7,
+      question: "Can I save pictures of receipts?",
+      answer:
+        "Yes, of course. You can snap a photo, scan a document and even attach a PDF file.",
+    },
+    {
+      id: 8,
+      question: "Can I use Siri?",
+      answer:
+        "Yeah! Adding transactions using Siri on the Apple Watch is the newest and most kickass feature yet.",
+    },
+    {
+      id: 9,
+      question: "Can I use import data from other apps?",
+      answer:
+        "Absolutely! You can import your financial data from any other app or software that you have. All you need is a CSV file.",
+    },
+    {
+      id: 10,
+      question: "Can I add Payees, Companies or Vendors?",
+      answer:
+        "Yes! You can attach a person, company or vendor to any transaction you add to MoneyCoach.",
+    },
+    {
+      id: 11,
+      question: "Can I add a location to my transactions?",
+      answer:
+        "Totally! You can attach a location to any of the transactions you add in the app.",
+    },
 
     // More questions...
   ]
