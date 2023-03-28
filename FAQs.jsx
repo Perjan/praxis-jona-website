9bd8ba4e20b89cea985c17f2e618e5ed417f0586
Perjan Duro <perjan.duro@me.com>
2023-03-28 10:44:26 +0200

    Adds faqs data

---
+++ FAQs.jsx
@@ -0,0 +1,16 @@
+const faqs = [
+    {
+      id: 1,
+      question: "What's the best thing about Switzerland?",
+      answer:
+        "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+        id: 2,
+        question: "What's the best thing about Switzerland?",
+        answer:
+          "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      },
+
+    // More questions...
+  ]
