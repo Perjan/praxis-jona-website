01cdc6ac4d49fe39b86873a6b8ae5735cb584aec
Perjan Duro <perjan.duro@me.com>
2023-03-30 15:08:08 +0200

    Fixes build issue

--- app/AppleLovesMoneyCoachSection.tsx
+++ app/AppleLovesMoneyCoachSection.tsx
@@ -1,3 +1,5 @@
+const description = "MoneyCoach has been featured multiple times worldwide on the App Stores for a number of years now. We have also been featured on Apple's website a couple of times. We made an appearance during both WWDC20 and WWDC21 keynotes. We also got a dedicated Developer Story on the App Store. Crazy, right?"
+
 export default function AppleLovesMoneyCoachSection() {
     return (
         <>
@@ -5,7 +7,7 @@ export default function AppleLovesMoneyCoachSection() {
                 <div className="py-24 px-6 sm:px-6 sm:py-32 lg:px-8">
                     <div className="mx-auto max-w-2xl text-center">
                         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Apple ❤️ MoneyCoach</h2>
-                        <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">MoneyCoach has been featured multiple times worldwide on the App Stores for a number of years now. We have also been featured on Apple's website a couple of times. We made an appearance during both WWDC20 and WWDC21 keynotes. We also got a dedicated Developer Story on the App Store. Crazy, right?</p>
+                        <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">{description}</p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">App Store Developer Spotlight</a>
                         </div>
