ed0eb5ce5fd6ca7e34633f0f7b7ac994fee41cfb
Perjan Duro <perjan.duro@me.com>
2023-04-04 15:09:38 +0200

    Adds header to features

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -158,6 +158,58 @@ const features = [
 export default function Features() {
   return (
     <>
+
+<div className="overflow-hidden">
+              <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                  <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">MoneyCoach is feature-packed
+                    </h1>
+                    <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">MoneyCoach sticks to what is essential for finance. The advanced features are out of the way until you actually need them.</p><a className="hover:text-gray-900" href="mailto:info@moneycoach.ai">info@moneycoach.ai</a>
+                  </div>
+                  <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                    <div
+                      className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                      <div className="relative">
+                        <Image width={176} height={264} src="/images/features/hero1.png" alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                      <div className="relative">
+                        <Image width={176} height={264} src="/images/features/hero2.png" alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
+                        </div>
+                      </div>
+                      <div className="relative">
+                        <Image width={176} height={264} src="/images/features/hero3.png" alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
+                        </div>
+                      </div>
+                    </div>
+                    <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+                      <div className="relative">
+                        <Image width={176} height={264} src="/images/features/hero4.png" alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
+                        </div>
+                      </div>
+                      <div className="relative">
+                        <Image width={176} height={264} src="/images/features/hero5.png" alt=""
+                          className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                        <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <ul
