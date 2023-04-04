6914131790f5336c5f48034f956d3024bc585e58
Krist Duro <krist.duro@icloud.com>
2023-04-04 11:33:36 +0200

    improves Apple Section

--- app/AppleLovesMoneyCoachSection.tsx
+++ app/AppleLovesMoneyCoachSection.tsx
@@ -1,28 +1,59 @@
+const title = "Apple ❤️ MoneyCoach"
 const description = "MoneyCoach has been featured multiple times worldwide on the App Stores for a number of years now. We have also been featured on Apple's website a couple of times. We made an appearance during both WWDC20 and WWDC21 keynotes. We also got a dedicated Developer Story on the App Store. Crazy, right?"
 
 export default function AppleLovesMoneyCoachSection() {
-    return (
-        <>
-            <div className="relative isolate overflow-hidden bg-gray-900">
-                <div className="py-24 px-6 sm:px-6 sm:py-32 lg:px-8">
-                    <div className="mx-auto max-w-2xl text-center">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Apple ❤️ MoneyCoach</h2>
-                        <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">{description}</p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a href="#" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">App Store Developer Spotlight</a>
-                        </div>
-                    </div>
-                </div>
-                <svg viewBox="0 0 1024 1024" className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
-                    <circle cx="512" cy="512" r="512" fill="url(#8d958450-c69f-4251-94bc-4e091a323369)" fill-opacity="0.7" />
-                    <defs>
-                        <radialGradient id="8d958450-c69f-4251-94bc-4e091a323369">
-                            <stop stop-color="#7775D6" />
-                            <stop offset="1" stop-color="#E935C1" />
-                        </radialGradient>
-                    </defs>
-                </svg>
+  return (
+    <div className="overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+          <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">{title}</h2>
+            <p className="mt-6 text-xl leading-8 text-slate-200">
+             {description}
+            </p>
+            <div className="mt-10 flex">
+              <a
+                href="https://apps.apple.com/is/story/id1591098240"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                App Store Developer Spotlight<span aria-hidden="true">&rarr;</span>
+              </a>
             </div>
-        </>
-    )
+          </div>
+          <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+            <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+              <img
+                src="/images/developerSpotlight.png"
+                alt=""
+                className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+              />
+            </div>
+            <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+              <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                <img
+                  src="/images/moneyCoachFeaturedAppStore.png"
+                  alt=""
+                  className=" w-[16rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+              <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+                <img
+                  src="/images/appOfTheDay.png"
+                  alt=""
+                  className=" w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+              <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+                <img
+                  src="/images/moneyCoachWWDC20.png"
+                  alt=""
+                  className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
