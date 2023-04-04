dda4d720297179834f53f6a458232733bf3616b3
feliche93 <felix.vemmer@gmail.com>
2023-04-04 20:33:11 +0200

    fix: fixes sending of form data

--- app/api/sendgrid/route.ts
+++ app/api/sendgrid/route.ts
@@ -7,9 +7,9 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 //send email using sendgrid from contact form
 export async function POST(request: Request) {
     const requestJson = await request.json()
-    
+
     console.log('sendgrid route');
-    console.log({requestJson});
+    console.log({ requestJson });
 
     const msg = {
         to: 'info@moneycoach.ai',
@@ -20,9 +20,9 @@ export async function POST(request: Request) {
     };
     try {
         const response = await sendgrid.send(msg);
-        console.log({response});
-        return NextResponse.json({response});
-    }   catch (error) {
+        console.log({ response });
+        return NextResponse.json({ response });
+    } catch (error) {
         console.error(error);
         if (error.response) {
             console.error(error.response.body)
