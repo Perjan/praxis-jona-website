75e3d0dbf4f6afc3cb946b581b54f07fa6b1c787
Perjan Duro <perjan.duro@me.com>
2023-04-04 22:36:05 +0200

    Refactors

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -20,8 +20,56 @@ const intermediatePosts = guidesPosts.filter(
   ].includes(post.slug)
 )
 
+const advancedPosts = guidesPosts.filter(
+  (post) => [
+    "how-to-use-moneycoach",
+    "how-to-start-fresh-with-moneycoach"
+  ].includes(post.slug)
+)
+
+const questionsAndAnswersPosts = guidesPosts.filter(
+  (post) => [
+    "how-to-use-moneycoach",
+    "how-to-start-fresh-with-moneycoach"
+  ].includes(post.slug)
+)
+
 function getWordStr(str, wordCount) {
   return str.split(/\s+/).slice(0, wordCount).join(" ");
+}
+
+function PostCard(post: Post) {
+  return (
+    <article key={post.slug} className="flex max-w-xl flex-col items-start justify-normal content-start">
+      <div className="flex items-center gap-x-4 text-xs">
+        <time dateTime={post.date} className="text-gray-500">
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+
+        {
+          post.categories?.map((item) => (
+            <a
+              key={item}
+              href={item}
+              className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+            >
+              {item}
+            </a>
+          ))
+        }
+
+      </div>
+      <div className="group relative">
+        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+          <a href={post.url}>
+            <span className="absolute inset-0" />
+            {post.title}
+          </a>
+        </h3>
+        <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{getWordStr(post.body.raw, 40)}</p>
+      </div>
+    </article>
+  )
 }
 
 // Returns a section with a list of posts and a section title
@@ -33,35 +81,7 @@ function PostSection({ posts, title }) {
         {
           posts
             .map((post) => (
-              <article key={post.slug} className="flex max-w-xl flex-col items-start justify-normal content-start">
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.date} className="text-gray-500">
-                    {format(parseISO(post.date), 'LLLL d, yyyy')}
-                  </time>
-
-                  {
-                    post.categories?.map((item) => (
-                      <a
-                        key={item}
-                        href={item}
-                        className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                      >
-                        {item}
-                      </a>
-                    ))
-                  }
-
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={post.url}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{getWordStr(post.body.raw, 40)}</p>
-                </div>
-              </article>
+              PostCard(post)
             ))}
       </div>
     </div>
@@ -80,6 +100,8 @@ export default function Home() {
         </div>
         <PostSection posts={beginnerPosts} title="Beginner" />
         <PostSection posts={intermediatePosts} title="Intermediate" />
+        <PostSection posts={advancedPosts} title="Advanced" />
+        <PostSection posts={questionsAndAnswersPosts} title="Questions and Answers" />
       </div>
     </div>
   )
