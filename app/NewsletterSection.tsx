82ee63e9a3a47960f82f2fdf1920812a16c2106a
Perjan Duro <perjan.duro@me.com>
2023-04-04 15:41:21 +0200

    Fixes newsletter button

--- app/NewsletterSection.tsx
+++ app/NewsletterSection.tsx
@@ -34,7 +34,7 @@ const [email, setEmail] = useState("");
                 type="email"
                 value={email}
                 required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-greenColor sm:text-sm sm:leading-6"
                 placeholder="Enter your email"
                 onChange={ (e) => {
                   const email = e.target.value;
@@ -45,7 +45,7 @@ const [email, setEmail] = useState("");
               <button
                 disabled={!isEmail(email)}
                 type="submit"
-                className="flex-none rounded-md bg-primary py-2.5 px-3.5 text-sm font-semibold text-black shadow-sm enabled:hover:bg-indigo-400 disabled:opacity-70 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex-none rounded-md bg-primary py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm enabled:hover:bg-primaryDarker disabled:opacity-70 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                 onClick={async () => {
                   // setButtonDisabled(true);
                   const response = await fetch('/api/mailchimp', {
