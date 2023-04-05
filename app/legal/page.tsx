d6f6b8c18e4468eed944dd551f3504a143fbbcaa
Perjan Duro <perjan.duro@me.com>
2023-04-05 08:46:24 +0200

    Adds legal stuff

---
+++ app/legal/page.tsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Post, allPosts } from 'contentlayer/generated'
+
+
+const legalPosts = allPosts.filter((post) => post.categories?.includes('legal'))
+
+function getWordStr(str, wordCount) {
+  return str.split(/\s+/).slice(0, wordCount).join(" ");
+}
+
+function PostCard(post: Post) {
+  return (
+    <>
+      <article key={post.slug} className="flex max-w-xl flex-col items-start justify-normal content-start">
+              <div className="flex items-center gap-x-4 text-xs">
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.legalUrl}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+
+              </div>
+            </article>
+    </>
+  )
+}
+
+// Returns a section with a list of posts and a section title
+function PostSection({ posts, title }) {
+  return (
+    <div className='mx-auto mt-10 max-w-2xl border-t border-gray-200 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none '>
+      <div className="grid grid-cols-1 gap-x-8 gap-y-16pt-10 lg:grid-cols-3">
+        {
+          posts
+            .map((post) => (
+              PostCard(post)
+            ))}
+      </div>
+    </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Legal</h1>
+        </div>
+        <PostSection posts={legalPosts} title="" />
+      </div>
+    </div>
+  )
+}
--- app/terms-of-use/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
-        <article className="prose lg:prose-xl">
-            <h1>Terms of Use</h1>
-        </article>
-        </div>
-    )
-    }
