195dfe7132e26f52b41fef4a87cf6cc1451058b0
Perjan Duro <perjan.duro@me.com>
2023-04-06 15:31:00 +0200

    Adds metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,47 @@ import { Metadata } from 'next';
 import { ReactToastWrapper } from './ToasterWrapper';
 
 export const metadata: Metadata = {
-  title: { 
-    default: "MoneyCoach - Budgeting App", 
-    template: "%s | MoneyCoach - Budgeting App" 
+  title: {
+    default: "MoneyCoach - Budgeting App",
+    template: "%s | MoneyCoach - Budgeting App"
   },
   description: "MoneyCoach is a personal finance app that helps you manage your money, budget, and track your spending.",
   twitter: {
     // handle: "@appmoneycoach"
     site: "@appmoneycoach",
     // cardType: "summary_large_image"
+  },
+  openGraph: {
+    title: 'MoneyCoach - Budgeting App',
+    url: 'https://moneycoach.ai',
+    images: [
+      {
+        url: 'https://moneycoach.ai/images/og-image.png',
+        width: 800,
+        height: 600,
+      }
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  appLinks: {
+
+    ios: {
+      app_name: 'MoneyCoach',
+      app_store_id: '989642198',
+      url: 'https://apps.apple.com/us/app/moneycoach-budget-spendings/id989642198',
+    }
   }
 }
 
@@ -27,10 +59,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ReactToastWrapper>
-        <Header />
-        {children}
-        <Footer />
-        <Analytics />
+          <Header />
+          {children}
+          <Footer />
+          <Analytics />
         </ReactToastWrapper>
       </body>
     </html>
