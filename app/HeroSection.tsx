cb09d4cfc0e6abd25b0becd536b92e6da0b2af92
Perjan Duro <perjan.duro@me.com>
2023-04-15 21:04:03 +0200

    Fixes formatting

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { 
+import {
     ChevronRightIcon,
     PlayCircleIcon,
     SparklesIcon
@@ -41,12 +41,12 @@ export default function HeroSection() {
                     <div className="mt-0 sm:mt-4 lg:mt-24 h-0 collapse sm:visible">
                         <Link href={whatsNewUrl} className="inline-flex space-x-6">
                             <span className="rounded-full inline-flex items-center space-x-2 bg-primary/10 px-3 py-1 text-sm font-semibold leading-6 text-primaryDarker ring-1 ring-inset ring-indigo-600/10">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
-</svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                                </svg>
 
-                            {"Just shipped v8.4.2"}
-                            <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                {"Just shipped v8.4.2"}
+                                <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                             </span>
                         </Link>
                     </div>
@@ -76,7 +76,7 @@ export default function HeroSection() {
                     </div>
                     <div className="grid gap-x-4 pr-6 grid-cols-2 mt-12 mb-10 w-[36rem]">
                         <div>
-                    <Image
+                            <Image
                                 className="rounded-md"
                                 src="/images/app-of-the-day.png"
                                 alt='MoneyCoach app screenshot'
@@ -86,7 +86,7 @@ export default function HeroSection() {
                             />
                         </div>
                         <div>
-                    <Image
+                            <Image
                                 className="rounded-md"
                                 src="/images/accolades-combo.png"
                                 alt='MoneyCoach app screenshot'
