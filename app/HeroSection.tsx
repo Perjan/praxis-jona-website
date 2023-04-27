5a373fcf68f486fba220791e8838cae8f7a0a63a
Krist Duro <krist.duro@icloud.com>
2023-04-27 13:27:51 +0200

    adds release notes

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -13,7 +13,7 @@ import HeroImage from "/public/images/moneycoach-all-devices.png"
 
 const title = "Modern Cash And Budget Tracker"
 const description = "Track all your cash spending, manage your personal budgets and reduce your financial stress. That's what MoneyCoach is all about."
-const whatsNewUrl = "/whats-new-in-moneycoach-8-4-3"
+const whatsNewUrl = "/whats-new-in-moneycoach-8-4-4"
 const downloadUrl = "https://apps.apple.com/us/app/moneycoach-budget-spendings/id989642198"
 const videoUrl = "https://www.youtube.com/watch?v=phpFfo80LPI&t=22s"
 const previewUrl = "https://www.youtube.com/shorts/rh5_8mVDx4Q"
@@ -51,7 +51,7 @@ export default function HeroSection() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
                                 </svg>
 
-                                {"Just shipped v8.4.3"}
+                                {"Just shipped v8.4.4"}
                                 <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                             </span>
                         </Link>
