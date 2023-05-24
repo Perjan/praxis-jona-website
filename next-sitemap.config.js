01ea3bc5c6b7ab1e02cf0b4616f4220c6780f560
Perjan Duro <perjan.duro@me.com>
2023-05-24 15:36:25 +0200

    Excludes non-canonicals from sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://moneycoach.ai',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+  siteUrl: 'https://moneycoach.ai',
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    "/company/press-kit",
+    "/legal/privacy-policy",
+    "/de",
+    "/it",
+    "/legal/credits",
+    "/legal/imprint",
+    "/legal/disclaimer",
+    "/legal/terms-of-use",
+    "/it/guide",
+    "/compare/monefy-alternative",
+    "/legal/moneyspaces-privacy-policy",
+    "/de/support",
+    "/de/einfuehrungen",
+    "/de/funktionen",
+    "/it/rapporti",
+    "/it/supporto",
+    "/de/blog",
+    "/compare/finanzguru-alternative",
+    "/it/blog",
+    "/de/ueber-uns",
+    "/it/chi-siamo",
+    "/de/faq",
+    "/it/domande-frequenti",
+    "/de/berichte",
+    "/legal",
+    "/it/funzioni",
+    "/compare/buddy-alternative",
+  ],
+
+  // ...other options
+}
