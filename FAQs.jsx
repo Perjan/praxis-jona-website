638b1431898eefa6fd669ea41c49dd17b78c2b22
Krist Duro <krist.duro@icloud.com>
2023-07-18 16:20:00 +0200

    adds faqs

--- FAQs.jsx
+++ FAQs.jsx
@@ -7,60 +7,78 @@ export const faqs = [
     },
     {
         id: 2,
-        question: "Is MoneyCoach private?",
+        question: "Can I share my data with my partner?",
         answer:
-          "Absolutely. We have the utmost respect for your privacy. Your information is safe and secure with MoneyCoach. And it stays on your device.",
+          "Yes! You can share data with your partner as long as you are both subscribed to MoneyCoach Premium.",
     },
     {
       id: 3,
+      question: "Can I share my subscription with my family members?",
+      answer:
+        "If you or a family member subscribes to a Family Plan subscription, everyone included in the family will become a premium user after restoring purchases. Keep in mind that it takes up to 1 hour for the purchase to become available to the other family members.",
+    },
+    {
+      id: 4,
+      question: "Can I use MoneyCoach on all my devices with one subscription?",
+      answer:
+        "Yes! If you subscribe to MoneyCoach Premium on your main device, you will be upgraded to Premium on all your other devices under the same Apple ID.",
+    },
+    {
+      id: 5,
+      question: "Is MoneyCoach private?",
+      answer:
+        "Absolutely. We have the utmost respect for your privacy. Your information is safe and secure with MoneyCoach. And it stays on your device.",
+    },
+    {
+      id: 6,
       question: "What currencies are available?",
       answer:
         "All of them. From the American Dollar to the Albanian lek. (Premium Feature)",
     },
     {
-        id: 4,
+        id: 7,
         question: "How secure is MoneyCoach?",
         answer:
           "Very secure. MoneyCoach is secured using Passcode, Touch ID or FaceID, so you can sleep safely… that is if Ethan Hunt is not around.",
     },
     {
-      id: 5,
+      id: 8,
       question: "How much does it cost?",
       answer:
         "We offer MoneyCoach Premium for our Pro users. If you are a normal user, you can still get all the most used features for free. But if you want to support future development and the team, you can subscribe to MoneyCoach Premium and unlock extra powerful features.",
     },
     {
-      id: 6,
+      id: 9,
       question: "Are there any ads?",
       answer:
         "Nope. We hate ads, just like you do.",
     },
     {
-      id: 7,
+      id: 10,
       question: "Can I save pictures of receipts?",
       answer:
         "Yes, of course. You can snap a photo, scan a document and even attach a PDF file.",
     },
     {
-      id: 8,
+      id: 11,
       question: "Can I use Siri?",
       answer:
         "Yeah! Adding transactions using Siri on the Apple Watch is the newest and most kickass feature yet.",
     },
     {
-      id: 9,
+      id: 12,
       question: "Can I use import data from other apps?",
       answer:
         "Absolutely! You can import your financial data from any other app or software that you have. All you need is a CSV file.",
     },
     {
-      id: 10,
+      id: 13,
       question: "Can I add Payees, Companies or Vendors?",
       answer:
         "Yes! You can attach a person, company or vendor to any transaction you add to MoneyCoach.",
     },
     {
-      id: 11,
+      id: 14,
       question: "Can I add a location to my transactions?",
       answer:
         "Totally! You can attach a location to any of the transactions you add in the app.",
@@ -78,61 +96,79 @@ export const faqs = [
         "MoneyCoach ti aiuta a tennere traccia dei contanti e dei tuoi budget. È stato progettato per darvi un controllo sulle vostre finanze personali. Vi aiuta a risparmiare di più e a spendere in modo più intelligente. E vi premia se raggiungete i vostri obiettivi. L'applicazione è nota anche come Money Coach o Money Coach App. Ma noi preferiamo MoneyCoach. CURIOSITÀ: Alcuni utenti ci hanno chiamato anche Money Couch 💰🛋️",
     },
     {
-        id: 2,
+      id: 2,
+      question: "Posso condividere i miei dati con il mio partner?",
+      answer:
+        "Si! Puoi condividere i dati con il tuo partner fintanto che entrambi siete abbonati a MoneyCoach Premium.",
+  },
+  {
+    id: 3,
+    question: "Posso condividere il mio abbonamento con i membri della mia famiglia?",
+    answer:
+      "Se tu o un membro della famiglia si abbona a un piano Famiglia, tutti i membri della famiglia diventeranno utenti premium dopo aver ripristinato gli acquisti. Tieni presente che è necessaria fino a 1 ora affinché l'acquisto diventi disponibile per gli altri membri della famiglia.",
+  },
+  {
+    id: 4,
+    question: "Posso usare MoneyCoach su tutti i miei dispositivi con un abbonamento?",
+    answer:
+      "Sì! Se ti abboni a MoneyCoach Premium sul tuo dispositivo principale, verrai aggiornato a Premium su tutti gli altri dispositivi con lo stesso ID Apple.",
+  },
+    {
+        id: 5,
         question: "MoneyCoach è privato?",
         answer:
           "Assolutamente sì. Abbiamo il massimo rispetto per la vostra privacy. Le vostre informazioni sono al sicuro con MoneyCoach. E rimangono sul vostro dispositivo.",
     },
     {
-      id: 3,
+      id: 6,
       question: "Quali valute sono disponibili?",
       answer:
         "Tutte. Dal dollaro americano al lek albanese. (Funzione Premium)",
     },
     {
-        id: 4,
+        id: 7,
         question: "Quanto è sicuro MoneyCoach?",
         answer:
           "Molto sicuro. MoneyCoach è protetto da Passcode, Touch ID o FaceID.",
     },
     {
-      id: 5,
+      id: 8,
       question: "Quanto costa?",
       answer:
         "Offriamo MoneyCoach Premium per i nostri utenti Pro. Se siete utenti normali, potete comunque ottenere gratuitamente tutte le funzioni più utilizzate. Ma se volete sostenere lo sviluppo futuro e il team, potete abbonarvi a MoneyCoach Premium e sbloccare altre potenti funzioni.",
     },
     {
-      id: 6,
+      id: 9,
       question: "Ci sono pubblicità?",
       answer:
         "No, non ci sono.",
     },
     {
-      id: 7,
+      id: 10,
       question: "Posso salvare le immagini delle ricevute?",
       answer:
         "Sì, certo. È possibile scattare una foto, scansionare un documento e persino allegare un file PDF.",
     },
     {
-      id: 8,
+      id: 11,
       question: "Posso usare Siri?",
       answer:
         "Sì! Puoi aggiungere movimenti utilizzando Siri e molte altre cose.",
     },
     {
-      id: 9,
+      id: 12,
       question: "Posso importare dati da altre app?",
       answer:
         "Assolutamente sì! È possibile importare i dati finanziari da qualsiasi altra app o software. Tutto ciò che serve è un file CSV.",
     },
     {
-      id: 10,
+      id: 13,
       question: "Posso aggiungere beneficiari, aziende o fornitori?",
       answer:
         "Sì! Potete aggiungere una persona, una azienda o un fornitore a qualsiasi movimento che aggiungete a MoneyCoach.",
     },
     {
-      id: 11,
+      id: 14,
       question: "Posso aggiungere un luogo ai miei movimenti?",
       answer:
         "Assolutamente sì! È possibile allegare un luogo a qualsiasi movimento aggiunto nell'app.",
@@ -147,61 +183,79 @@ export const faqs = [
         "MoneyCoach ist ein moderner Bargeld- und Budget-Tracker. Entwickelt, um dir einen Überblick über deine persönlichen Finanzen zu verschaffen. Er hilft dir, mehr zu sparen und intelligenter auszugeben. Und belohnt dich, wenn du deine Ziele erreichst. Die App ist auch als Money Coach oder Money Coach App bekannt. Wir bevorzugen jedoch MoneyCoach. FUN FACT: Es gibt einige Nutzer, die uns auch Money Couch genannt haben 💰🛋️",
     },
     {
-        id: 2,
+      id: 2,
+      question: "Kann ich meine Daten mit meinem Partner teilen?",
+      answer:
+        "Ja! Du kannst deine Daten mit deinem Partner teilen, solange ihr beide MoneyCoach Premium abonniert haben.",
+  },
+  {
+    id: 3,
+    question: "Kann ich mein Abonnement mit meinen Familienmitgliedern teilen?",
+    answer:
+      "Wenn du oder ein Familienmitglied ein Familienplan-Abonnement abschließen, werden alle Mitglieder der Familie nach der Wiederherstellung der Einkäufe zum Premium-Benutzer. Bitte beachten, dass es bis zu 1 Stunde dauern kann, bis der Kauf für die anderen Familienmitglieder verfügbar ist.",
+  },
+  {
+    id: 4,
+    question: "Kann ich MoneyCoach auf allen meinen Geräten mit einem Abonnement verwenden?",
+    answer:
+      "Ja! Wenn du MoneyCoach Premium auf deinem Hauptgerät abonnierst, werden deine anderen iOS & Mac Geräten unter derselben Apple ID auf Premium aktualisiert.",
+  },
+    {
+        id: 5,
         question: "Ist MoneyCoach privat?",
         answer:
           "Auf jeden Fall. Wir haben den größten Respekt vor deiner Privatsphäre. Deine Daten sind bei MoneyCoach sicher und geschützt. Und sie bleiben auf deinem Gerät.",
     },
     {
-      id: 3,
+      id: 6,
       question: "Welche Währungen gibt es?",
       answer:
         "Alle von ihnen. Vom amerikanischen Dollar bis zum albanischen Lek. (Premium Feature)",
     },
     {
-        id: 4,
+        id: 7,
         question: "Wie sicher ist MoneyCoach?",
         answer:
           "Sehr sicher. MoneyCoach ist mit Passcode, Touch ID oder FaceID gesichert.",
     },
     {
-      id: 5,
+      id: 8,
       question: "Wie viel kostet es?",
       answer:
         "Wir bieten MoneyCoach Premium für unsere Pro-Nutzer an. Wenn du ein normaler Nutzer bist, kannst du die meistgenutzten Funktionen weiterhin kostenlos nutzen. Aber wenn du die zukünftige Entwicklung und das Team unterstützen willst, kannst du MoneyCoach Premium abonnieren und zusätzliche leistungsstarke Funktionen freischalten.",
     },
     {
-      id: 6,
+      id: 9,
       question: "Gibt es Werbung?",
       answer:
         "Nö. Wir hassen Werbung, genau wie du.",
     },
     {
-      id: 7,
+      id: 10,
       question: "Kann ich Bilder von Belegen speichern?",
       answer:
         "Ja, natürlich. Du kannst ein Foto machen, ein Dokument scannen und sogar eine PDF-Datei anhängen.",
     },
     {
-      id: 8,
+      id: 11,
       question: "Kann ich Siri benutzen?",
       answer:
         "Yeah! Das Hinzufügen von Transaktionen mit Siri auf der Apple Watch ist die neueste und bisher tollste Funktion.",
     },
     {
-      id: 9,
+      id: 12,
       question: "Kann ich Importdaten aus anderen Apps verwenden?",
       answer:
         "Aber sicher! Du kannst deine Finanzdaten aus jeder anderen App oder Software importieren, die du hast. Alles, was du brauchst, ist eine CSV-Datei.",
     },
     {
-      id: 10,
+      id: 13,
       question: "Kann ich Zahlungsempfänger, Unternehmen oder Lieferanten hinzufügen?",
       answer:
         "Ja! Du kannst jeder Transaktion, die du in MoneyCoach hinzufügst, eine Zahlungsempfänger, ein Unternehmen oder einen Lieferanten zuordnen.",
     },
     {
-      id: 11,
+      id: 14,
       question: "Kann ich einen Ort zu meinen Transaktionen hinzufügen?",
       answer:
         "Vollkommen! Du kannst jedem Vorgang, den du in der App hinzufügst, einen Standort zuweisen.",
