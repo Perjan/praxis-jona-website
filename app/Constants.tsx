eb1a26c5ed027788b31ca8ab8b7358bbe13e7f35
Krist Duro <krist.duro@icloud.com>
2023-07-25 10:08:29 +0200

    adds comparison article

--- app/Constants.tsx
+++ app/Constants.tsx
@@ -1,3 +1,4 @@
 export const Constants = {
-    downloadUrl: "https://apps.apple.com/app/apple-store/id989642198?pt=118449936&ct=website&mt=8"
+    downloadUrl: "https://apps.apple.com/app/apple-store/id989642198?pt=118449936&ct=website&mt=8",
+    msDownloadUrl: "https://apps.apple.com/app/apple-store/id1633780211?pt=118449936&ct=website&mt=8"
 }
