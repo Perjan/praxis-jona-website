dfc86b27185cc4e8474b34b70dc231bed1ed3274
Perjan Duro <perjan.duro@me.com>
2023-08-23 14:37:12 +0200

    Fixes stuff

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
 
-export const config = { matcher: '/appSettings' };
+export const config = { matcher: '/appsettings' };
 
 export async function middleware() {
-  const greeting = await get('appSettings');
+  const appSettings = await get('appSettings');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
-  return NextResponse.json(greeting);
+  return NextResponse.json(appSettings["rtst"]);
 }
