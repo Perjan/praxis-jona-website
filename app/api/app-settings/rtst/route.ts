7ab92f8f79f0f6a81bd68c655029477b9ec7424b
Perjan Duro <perjan.duro@me.com>
2023-09-08 20:43:42 +0200

    Updates rtst api to download the latest apps

--- app/api/app-settings/rtst/route.ts
+++ app/api/app-settings/rtst/route.ts
@@ -7,5 +7,26 @@ export async function GET() {
     const appSettings = await get('appSettings');
     // NextResponse.json requires at least Next v13.1 or
     // enabling experimental.allowMiddlewareResponseBody in next.config.js
-    return NextResponse.json(appSettings["rtst"]);
+    const result = appSettings["rtst"];
+    result["apps"] = appEntities;
+    
+    return NextResponse.json(result);
+    // return NextResponse.json(appSettings["rtst"]);
 }
+
+// An entity in typesscript called AppPickerEntity
+// Path: app/api/app-settings/rtst/entity.ts
+export interface AppPickerEntity {
+    bundleId: string;
+    name: string;
+    color: string;
+}
+
+// An array of AppPickerEntity
+const appEntities: AppPickerEntity[] = [
+    {bundleId: "co.supertop.Castro-2", name: "Castro Podcast Player", color: "#02BD8A"},
+    {bundleId: "fm.overcast.overcast", name: "Overcast", color: "#FF8733"},
+    {bundleId: "com.pandora", name: "Pandora", color: "#7726B7"},
+    {bundleId: "com.audible.iphone", name: "Audible", color: "#FD8C08"},
+    {bundleId: "com.soundcloud.TouchApp", name: "SoundCloud", color: "#000000"},
+];
