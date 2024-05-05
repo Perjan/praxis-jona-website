cf6a65c4b504e5f51a104f6d9ce8e8de08a98bb2
Perjan Duro <perjan.duro@me.com>
2024-05-05 14:41:06 +0200

    Fixes domain name

--- app/Constants.tsx
+++ app/Constants.tsx
@@ -1,6 +1,6 @@
 export const Constants = {
     appName: "Praxis Jona",
-    baseUrl: "https://praxis-jona.de",
+    baseUrl: "https://praxisjona.de",
     keywords: ["praxis jona", "berlin praxis", "jona praxis"],
     umamiId: "cc9a5b16-c893-492f-af55-e6b79a844358",
     dataDomain: "praxisjona.de"
