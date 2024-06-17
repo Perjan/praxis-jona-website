2967fdacbc175ba0fd7098d9ab7b8c63f5529789
Perjan Duro <perjan.duro@me.com>
2024-06-17 13:18:24 +0200

    refactor: Update contact section with corrected text and formatting

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h1 className="text-3xl font-semibold tracking-tight font-serif text-primary sm:text-4xl">{title}</h1>
-            <h2 className="mt-2 text-lg leading-8 text-primaryLighter">"Praxis Jona - Gemeinschaftspraxis für Allgemeinmedizin, innere Medizin und Naturheilkunde.</h2>
+            <h2 className="mt-2 text-lg leading-8 text-primaryLighter">Praxis Jona - Gemeinschaftspraxis für Allgemeinmedizin, innere Medizin und Naturheilkunde.</h2>
 
             <h2 className="mt-2 text-lg leading-8 text-primaryLighter">Nutzen Sie unsere Online Services bequem von zuhause aus oder nehmen Sie gerne direkt Kontakt zu uns auf.</h2>
           </div>
