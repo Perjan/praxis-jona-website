cd7354b9d253cdc19a3b2331f922a5405dc1a790
Krist Duro <krist.duro@icloud.com>
2024-06-17 14:11:24 +0200

    change

--- app/schwerpunkte/schilddruse/page.tsx
+++ app/schwerpunkte/schilddruse/page.tsx
@@ -72,8 +72,20 @@ export default function Page() {
                             <li>Struppiges, sprödes Haar</li>
                         </ul>
 
+                        <p className="text-3xl mt-16 font-serif font-semibold leading-8 text-primaryLighter">Diagnose</p>
+                        <p className="mt-2 text-lg leading-8 text-primaryLighter">Die Schilddrüsendiagnostik ist entscheidend, um solche Erkrankungen frühzeitig zu erkennen und zu behandeln. Sie besteht aus:</p>
+
+                        <p className="text-xl mt-2 font-serif font-semibold leading-8 text-primaryLighter">Blutuntersuchungen</p>
+                        <p className="mt-2 text-lg leading-8 text-primaryLighter"> Überprüfung der Schilddrüsenhormone im Blut, um die Funktion der Schilddrüse zu beurteilen. Der TSH-Spiegel im Blut reagiert äußerst sensibel auf Veränderungen der Schilddrüsenhormonspiegel. Daher ist das im Serum gemessene TSH ein wichtiger Parameter zur Beurteilung der Schilddrüsenfunktion und der erste Schritt in der klinischen Diagnostik der Schilddrüse.</p>
+
+                        <p className="text-xl mt-2 font-serif font-semibold leading-8 text-primaryLighter">Ultraschalluntersuchung</p>
+                        <p className="mt-2 text-lg leading-8 text-primaryLighter">Eine bildgebende Methode, um die Größe, Struktur und mögliche Knoten oder Zysten in der Schilddrüse zu erkennen.</p>
+
+                        <p className="text-xl mt-2 font-serif font-semibold leading-8 text-primaryLighter">Schilddrüsenszintigraphie</p>
+                        <p className="mt-2 text-lg leading-8 text-primaryLighter">Die Untersuchung wird nur bei verdächtigen Veränderungen im Ultraschall empfohlen.</p>
+
                     </SectionWithColor>
-                    <SectionWithColor backgroundClassName='bg-tealColor'>
+                    {/* <SectionWithColor backgroundClassName='bg-tealColor'>
                         
                         <p className="text-3xl mt-6 font-serif font-semibold leading-8 text-primaryLighter">Diagnose</p>
                         <p className="mt-2 text-lg leading-8 text-primaryLighter">Die Schilddrüsendiagnostik ist entscheidend, um solche Erkrankungen frühzeitig zu erkennen und zu behandeln. Sie besteht aus:</p>
@@ -86,7 +98,7 @@ export default function Page() {
 
                         <p className="text-xl mt-2 font-serif font-semibold leading-8 text-primaryLighter">Schilddrüsenszintigraphie</p>
                         <p className="mt-2 text-lg leading-8 text-primaryLighter">Die Untersuchung wird nur bei verdächtigen Veränderungen im Ultraschall empfohlen.</p>
-                    </SectionWithColor>
+                    </SectionWithColor> */}
                 </div>
             </div>
         </>
