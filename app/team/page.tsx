2c64ee3e5b03eb323fd12559bf706ec881845c68
Perjan Duro <perjan.duro@me.com>
2024-06-30 17:55:09 -0400

    refactor: Update page component and add utility function

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { cn } from 'app/lib/utils';
 
 const people = [
   {
@@ -36,11 +37,12 @@ export default function Example() {
               <button onClick={() => setExpanded(!expanded)} className="bg-primaryDarker px-4 py-2 rounded-xl hover:bg-primaryLighter text-white font-semibold mt-4 transition duration-300 ease-in-out">
                 {expanded ? 'Schließen' : 'Weiterlesen'}
               </button>
-              <div style={{
-                maxHeight: expanded ? '600px' : '0', // Adjust '500px' as needed based on content size
-                overflow: 'hidden',
-                transition: 'max-height 0.5s ease-in-out'
-              }}>
+              <div 
+              className={
+                cn(expanded ? "max-h-auto" : "max-h-0",
+                    "overflow-hidden transition duration-500 ease-in-out")
+            }
+              >
                 <h2 className="mt-12 text-xl font-serif tracking-tight text-primary sm:text-2xl">Philosophie</h2>
                 <p className="mt-6 text-lg leading-8 text-primaryLighter">
                   "In meiner täglichen beruflichen Praxis ist es von herausragender Bedeutung für mich, einen umfassenden und ganzheitlichen Ansatz bei der Betrachtung des Menschen einzunehmen. Ich lege großen Wert darauf, nicht nur isolierte Symptome oder Krankheitsbilder zu analysieren, sondern vielmehr das Individuum in seiner Gesamtheit zu verstehen. Dies umfasst physische, psychische und soziale Aspekte, die alle in Verbindung stehen und einen Einfluss auf die Gesundheit und das Wohlbefinden des Einzelnen haben können. Mein Ziel ist es, durch diesen Blick eine effektive und individuell angepasste Betreuung und Unterstützung zu gewährleisten, um die bestmöglichen Ergebnisse für die Gesundheit meiner Patienten zu erzielen."
