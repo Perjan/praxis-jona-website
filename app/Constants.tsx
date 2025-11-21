fcdddf96fbf7e5fe57cc46b50b6aa5006f8ddc78
Krist Duro <krist.duro@icloud.com>
2025-11-21 14:31:14 +0100

    change aktuelle

--- app/Constants.tsx
+++ app/Constants.tsx
@@ -31,14 +31,14 @@ export const Constants = {
 export const NewsMessages = {
     de: [
         {
-            title: "Wichtige Praxisinformation – Weihnachtsschließzeiten",
-            message: "Liebe Patientinnen und Patienten,\n\nunsere Praxis bleibt vom 16. Dezember 2025 bis einschließlich 4. Januar 2026 geschlossen.\n\nDie Vertretungspraxis wird in Kürze bekannt gegeben.\n\nVielen Dank für Ihr Verständnis.\nIhr Praxisteam"
+            title: "Schließzeiten über die Feiertage",
+            message: "Unsere Praxis bleibt vom 16.12.2025 bis 02.01.2026 geschlossen.\nAb 05.01.2026 sind wir wieder wie gewohnt für Sie da.\nIn dringenden Fällen übernehmen folgende Praxen die Vertretung:\n\n<strong>Vertretung am 16.–17.12.2025 & 22.12.2025 (8–14 Uhr)</strong>\n<strong>Medizinisches Versorgungszentrum</strong>\n<strong>KfH-Gesundheitszentrum Berlin-Mitte</strong>\nGroße Hamburger Straße 5–11\n10115 Berlin-Mitte\nTel.: 030 3675 10070\n\n<strong>Vertretung vom 16.–30.12.2025</strong>\n<strong>Praxis Dr. Spieler</strong>\nPaul-Robinson-Straße 6\n10437 Berlin\nMo, Di, Do, Fr: 8–10 Uhr\nDi zusätzlich: 14–17 Uhr\nTel.: 030 2352 7702\n\n<strong>Vertretung am 02.01.2026 (8–10 Uhr)</strong>\n<strong>SPP Mitte – Dr. med. Grimm, Dr. med. Klausen, Dr. med. Wagner</strong>\nLinienstraße 127\n10115 Berlin\nTel.: 030 282 5052"
         }
     ],
     en: [
         {
-            title: "Important Practice Information – Christmas Closing Times",
-            message: "Dear patients,\n\nOur practice will be closed from December 16, 2025 to January 4, 2026, inclusive.\n\nThe substitute practice will be announced shortly.\n\nThank you for your understanding.\nYour practice team"
+            title: "Holiday Closing Times",
+            message: "Our practice will be closed from December 16, 2025 to January 2, 2026.\nFrom January 5, 2026, we will be back for you as usual.\nIn urgent cases, the following practices will provide representation:\n\n<strong>Representation on Dec. 16–17, 2025 & Dec. 22, 2025 (8 AM–2 PM)</strong>\n<strong>Medical Care Center</strong>\n<strong>KfH Health Center Berlin-Mitte</strong>\nGroße Hamburger Straße 5–11\n10115 Berlin-Mitte\nTel.: 030 3675 10070\n\n<strong>Representation from Dec. 16–30, 2025</strong>\n<strong>Practice Dr. Spieler</strong>\nPaul-Robinson-Straße 6\n10437 Berlin\nMon, Tue, Thu, Fri: 8–10 AM\nTue additionally: 2–5 PM\nTel.: 030 2352 7702\n\n<strong>Representation on Jan. 2, 2026 (8–10 AM)</strong>\n<strong>SPP Mitte – Dr. med. Grimm, Dr. med. Klausen, Dr. med. Wagner</strong>\nLinienstraße 127\n10115 Berlin\nTel.: 030 282 5052"
         }
     ]
 }
